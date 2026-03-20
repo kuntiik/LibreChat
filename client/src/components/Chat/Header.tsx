5cdb7c0c792e4372cfebbe9dffc5f29dfc94b503
Vojtech Janecek <vojtech.janecek@mattoni.cz>
2026-03-20 10:55:14 +0100

    fix: center llm selector button

--- client/src/components/Chat/Header.tsx
+++ client/src/components/Chat/Header.tsx
@@ -38,7 +38,7 @@ export default function Header() {
   const isSmallScreen = useMediaQuery('(max-width: 768px)');
 
   return (
-    <div className="via-presentation/70 md:from-presentation/80 md:via-presentation/50 2xl:from-presentation/0 pointer-events-none absolute top-0 z-10 flex h-20 w-full items-start justify-between bg-gradient-to-b from-presentation to-transparent px-2 font-semibold text-text-primary 2xl:via-transparent">
+    <div className="via-presentation/70 md:from-presentation/80 md:via-presentation/50 2xl:from-presentation/0 pointer-events-none absolute top-0 z-10 flex h-20 w-full items-center justify-between bg-gradient-to-b from-presentation to-transparent px-2 font-semibold text-text-primary 2xl:via-transparent">
       <div className="hide-scrollbar pointer-events-auto flex h-16 w-full items-center justify-between gap-2 overflow-x-auto">
         <div className="mx-1 flex h-full items-center">
           <AnimatePresence initial={false}>
